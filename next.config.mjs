647c9caf2b46ae6dcc1c2af52441e1eeca42e9fa
ghero56 <arcigaguzmanfernando@gmail.com>
2025-02-02 08:56:46 -0600

    refactor: node.js migration

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/* 
+* @type {import('next').NextConfig} 
+* 
+*/
+const nextConfig = {
+    output: 'standalone',
+    distDir: 'build',
+    images: {
+        unoptimized: false,
+    },
+};
+
+export default nextConfig;
